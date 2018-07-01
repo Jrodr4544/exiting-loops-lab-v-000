6802d47e4f157a99827742cde87827a344bd632b
Jelson Rodriguez <jelson.rodriguez1012@gmail.com>
2018-07-01 15:56:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+var breakOut = (array, changeValue, stopValue) => {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === stopValue) {
+      return array
+    }
+    array[i] = changeValue
+  }
+}
+
+var keepGoing = (array, changeValue, skipValue) => {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === skipValue) {
+        continue;
+      }
+      array[i] = changeValue
+    }
+    return array
+}
+
+var findBy = (array, findFn) => {
+  array.forEach(function(element) {
+   if (element === findFn) {
+     return element
+   } 
+  })
+  return null
+}
+
+// var findFn = (element) => {
+//     // return (element === value ? element : null)
+//     return element
+// }
